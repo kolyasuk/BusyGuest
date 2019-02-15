53d084119448636b2b43ddd25128e37a04b3239a
kolyasuk <dzioba99@gmail.com>
2019-02-15 21:06:21 +0200

    Initial commit

---
+++ src/main/java/iful/edu/bg/BusyGuestApplication.java
@@ -0,0 +1,14 @@
+package iful.edu.bg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusyGuestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusyGuestApplication.class, args);
+	}
+
+}
+
